8ca1ed06d0dec7f6448e4a952cce43991c706d01
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-07-11 01:34:00 +0530

    added fff

---
+++ fff/fffrc.sh
@@ -0,0 +1,10 @@
+export FFF_FAV2="/home/anupam/Downloads"
+export FFF_FAV4="/media/anupam/881f3c4e-63e4-4d5d-a149-e736788e2134/entertainment/anime"
+export FFF_FAV6="/home/anupam/Documents/Books"
+export FFF_FAV1="/home/anupam/Videos/video-tutorial/opengl_tutorial"
+if [ ! -d "/tmp/test" ]; then
+    mkdir "/tmp/test"
+fi
+
+export FFF_FAV8="/tmp/test"
+
