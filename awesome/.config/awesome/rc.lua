5d8ff6405e560d1dcf19c25fc1863a75ab31456a
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-10-24 12:41:45 +0530

    minor changes

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -58,7 +58,7 @@ beautiful.init("/home/anupam/.config/awesome/themes/default.lua")
 beautiful.get().wallpaper="/home/anupam/Pictures/wallpapers/minimal/255909-vertical-minimalistic-wallpaper-3840x2160-images.jpg"
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -768,8 +768,8 @@ local autorunApps =
 {
   "/usr/bin/picom -b",
   "/usr/bin/nitrogen --restore &",
-  "/usr/bin/copyq &",
-  "/opt/activitywatch/aw-qt &",
+  --  "/usr/bin/copyq &",
+  --  "/opt/activitywatch/aw-qt &",
   -- "/usr/bin/greenclip daemon &",
   --  "/usr/bin/clipmenud &",
 }
