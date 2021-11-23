0e7ee3fe08b379b73d68d0c28bb403647053133e
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-11-23 19:43:15 +0530

    cht.sh modification
    using less as a pager
    new languages and commands

--- scripts/scripts/cht.sh
+++ scripts/scripts/cht.sh
@@ -9,8 +9,9 @@ read -p "Enter Query: " query
 if grep -qs "$selected" $lang_file; then
 	query=`echo $query | tr ' ' '+'`
     # curl cht.sh/$selected/$query
-	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+	# tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less -R"
 else
     # curl cht.sh/$selected~$query
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
 fi
