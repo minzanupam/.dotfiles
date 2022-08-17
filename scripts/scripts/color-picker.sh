7ce4bcf73bde694748242a7dca1d9c36f26b6be5
Anupam Ashish Minz <anupamminz300am@gmail.com>
2022-08-17 13:02:30 +0530

    improved color picker script

--- scripts/scripts/color-picker.sh
+++ scripts/scripts/color-picker.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-xcolor | xclip -sel clip
+color_picked=`xcolor | tr A-Z a-z`
+new_color=`yad --color --init-color=$color_picked | tr A-Z a-z`
+
+if [ $new_color != "" ]; then
+	color_picked=$new_color
+fi
+
+xclip -sel clip <<< $color_picked
