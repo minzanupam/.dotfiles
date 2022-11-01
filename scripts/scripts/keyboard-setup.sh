2e8961e57bfda85648b4907e5931c3d9964655e6
Anupam Ashish Minz <anupamminz300am@gmail.com>
2022-11-01 23:02:34 +0530

    keyboard config

--- scripts/scripts/keyboard-setup.sh
+++ scripts/scripts/keyboard-setup.sh
@@ -1,2 +1,4 @@
-setxkbmap real-prog-dvorak -print -option caps:swapescape | xkbcomp -I$HOME/.config/xkb - $DISPLAY
+# setxkbmap real-prog-dvorak -print -option caps:swapescape | xkbcomp -I$HOME/.config/xkb - $DISPLAY
+setxkbmap -option caps:swapescape
+xmodmap /home/anupam/.config/xkb/xmodmap/real-prog-dvorak-xmodmap
 xset r rate 400 25
