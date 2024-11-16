356443956b5198e7031c133563b40cc4a24ca4ca
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-11-16 11:25:11 +0530

    add keybindings to change screen
    awesome wm

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -362,9 +362,9 @@ globalkeys = gears.table.join(
 	awful.key({ modkey }, "k", function()
 		awful.client.focus.byidx(-1)
 	end, { description = "focus previous by index", group = "client" }),
-	awful.key({ modkey }, "w", function()
-		mymainmenu:show()
-	end, { description = "show main menu", group = "awesome" }),
+	-- awful.key({ modkey }, "w", function()
+	-- 	mymainmenu:show()
+	-- end, { description = "show main menu", group = "awesome" }),
 
 	-- Layout manipulation
 	awful.key({ modkey, "Shift" }, "j", function()
@@ -373,14 +373,20 @@ globalkeys = gears.table.join(
 	awful.key({ modkey, "Shift" }, "k", function()
 		awful.client.swap.byidx(-1)
 	end, { description = "swap with previous client by index", group = "client" }),
-	awful.key({ modkey, "Control" }, "j", function()
-		awful.screen.focus_relative(1)
-	end, { description = "focus the next screen", group = "screen" }),
-	awful.key({ modkey, "Control" }, "k", function()
-		awful.screen.focus_relative(-1)
-	end, { description = "focus the previous screen", group = "screen" }),
-	awful.key({ modkey }, "u", awful.client.urgent.jumpto,
-		{ description = "jump to urgent client", group = "client" }),
+	awful.key({ modkey }, "w", function()
+		awful.screen.focus(1)
+	end, {}),
+	awful.key({ modkey }, "e", function()
+		awful.screen.focus(2)
+	end, {}),
+	-- awful.key({ modkey, "Control" }, "j", function()
+	-- 	awful.screen.focus_relative(1)
+	-- end, { description = "focus the next screen", group = "screen" }),
+	-- awful.key({ modkey, "Control" }, "k", function()
+	-- 	awful.screen.focus_relative(-1)
+	-- end, { description = "focus the previous screen", group = "screen" }),
+	-- awful.key({ modkey }, "u", awful.client.urgent.jumpto,
+	-- 	{ description = "jump to urgent client", group = "client" }),
 	awful.key({ modkey }, "Tab", function()
 		awful.client.swap.byidx(1)
 		awful.client.focus.byidx(-1)
